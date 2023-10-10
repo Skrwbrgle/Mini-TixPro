42f2485d0be581a086085129c834436f06a24e45
Indra Oki Sandy <indraokisandy1@gmail.com>
2023-10-10 21:52:17 +0700

    Add routing of Users and admin (not completed)

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,5 +3,11 @@ const userRoutes = require("express").Router();
 userRoutes.get("/", (req, res) => {});
 userRoutes.get("/login", (req, res) => {});
 userRoutes.post("/register", (req, res) => {});
+userRoutes.get("/dashboard", (req, res) => {});
+
+userRoutes.put("/update/:id", (req, res) => {});
+userRoutes.delete("/delete/:id", (req, res) => {});
+
+userRoutes.get("/logout", (req, res) => {});
 
 module.exports = userRoutes;
