9ec716ef50020b405fec1801d6cfc6aa3c890041
Skrwbrgle <kenpachiarts@gmail.com>
2023-10-12 22:24:15 +0700

    Add Booking

--- server/controllers/BookingController.js
+++ server/controllers/BookingController.js
@@ -1,3 +1,37 @@
-class BookingController {}
+const { booking, payment, user } = require("../models");
+
+class BookingController {
+  static async createBooking(req, res) {
+    try {
+      const { id, role, identification } = req.userData;
+      const resPayment = await payment.findAll({
+        where: { userId: +id, status: "approved" },
+      });
+
+      if (role === "1") {
+        if (resPayment.langth !== 0) {
+          resPayment.forEach((e) => {
+            booking.create({
+              userId: +id,
+              eventId: e.eventId,
+              paymentId: e.id,
+              barcode: `${identification}-${e.status}`,
+            });
+          });
+
+          res.status(200).json({ message: `Has been Booked!` });
+        } else {
+          res.status(404).json({ message: `Cannot create Booking!` });
+        }
+      } else {
+        res.status(403).json({
+          message: `Access denided`,
+        });
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
+}
 
 module.exports = BookingController;
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,10 +2,12 @@ const UserController = require("./UserController");
 const EventController = require("./EventController");
 const SeatController = require("./SeatController");
 const PaymentController = require("./PaymentController");
+const BookingController = require("./BookingController");
 
 module.exports = {
   EventController,
   SeatController,
   UserController,
   PaymentController,
+  BookingController,
 };
